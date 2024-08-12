658d727b9fbc88b94db7f5e66722ea159d298c10
Emmanuel <opensoftweb@gmail.com>
2024-08-12 15:16:16 +0100

    chore: optimize currency lib

--- src/libraries/Currency.sol
+++ src/libraries/Currency.sol
@@ -8,6 +8,8 @@ import {IERC20} from "../interfaces/external/IERC20.sol";
  * @title Currency
  * @notice A library to safely manage transfer of both native
  * and non-native token in a gas optimised manner
+ * @author Iphyman
+ * @author Modified from Solady https://github.com/Vectorized/solady/blob/main/src/utils/SafeTransferLib.sol
  */
 library Currency {
     using Currency for address;
@@ -20,6 +22,9 @@ library Currency {
     /// @notice Thrown when an ERC20 transfer fails
     error ERC20TransferFailed();
 
+    /// @notice Thrown when ERC20 transferFrom fails
+    error TransferFromFailed();
+
     /**
      * @notice A function to handle outbound native and non-native asset transfer
      *
@@ -29,18 +34,53 @@ library Currency {
      */
     function transfer(address currency, address to, uint256 amount) internal {
         if (currency.isNative()) {
-            assembly ("memory-safe") {
-                // Transfer the ETH and revert if it fails.
-                if iszero(call(gas(), to, amount, 0x00, 0x00, 0x00, 0x00)) {
-                    mstore(0x00, 0xf4b3b1bc) // `NativeTransferFailed()`.
-                    revert(0x1c, 0x04)
-                }
-            }
+            safeTransferETH(to, amount);
         } else {
-            IERC20 token = IERC20(currency);
-            bool success = _callOptionalReturnBool(token, abi.encodeCall(token.transfer, (to, amount)));
+            safeTransfer(currency, to, amount);
+        }
+    }
 
-            if (!success) revert ERC20TransferFailed();
+    /**
+     * @dev Send `amount` in wei to `to`
+     *
+     * @param to the address to send `amount`
+     * @param amount the value amount been sent
+     */
+    function safeTransferETH(address to, uint256 amount) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(call(gas(), to, amount, codesize(), 0x00, codesize(), 0x00)) {
+                mstore(0x00, 0xf4b3b1bc) // `NativeTransferFailed()`.
+                revert(0x1c, 0x04)
+            }
+        }
+    }
+
+    /**
+     * @dev handles safely ERC20 transfer from contract to `to`
+     *
+     * @param currency the token address
+     * @param to the recipient address
+     * @param amount the value to transfer
+     */
+    function safeTransfer(address currency, address to, uint256 amount) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x14, to) // Store the `to` argument.
+            mstore(0x34, amount) // Store the `amount` argument.
+            mstore(0x00, 0xa9059cbb000000000000000000000000) // `transfer(address,uint256)`.
+            // Perform the transfer, reverting upon failure.
+            if iszero(
+                and(
+                    // The arguments of `and` are evaluated from right to left.
+                    or(eq(mload(0x00), 1), iszero(returndatasize())), // Returned 1 or nothing.
+                    call(gas(), currency, 0, 0x10, 0x44, 0x00, 0x20)
+                )
+            ) {
+                mstore(0x00, 0xf27f64e4) // `ERC20TransferFailed()`.
+                revert(0x1c, 0x04)
+            }
+            mstore(0x34, 0) // Restore the part of the free memory pointer that was overwritten.
         }
     }
 
@@ -52,11 +92,33 @@ library Currency {
      * @param to receipient address
      * @param amount value to transfer
      */
-    function safeTransferFrom(address currency, address from, address to, uint256 amount) internal {
-        IERC20 token = IERC20(currency);
-        bool success = _callOptionalReturnBool(token, abi.encodeCall(token.transferFrom, (from, to, amount)));
-
-        if (!success) revert ERC20TransferFailed();
+    function safeTransferFrom(
+        address currency,
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let m := mload(0x40) // Cache the free memory pointer.
+            mstore(0x60, amount) // Store the `amount` argument.
+            mstore(0x40, to) // Store the `to` argument.
+            mstore(0x2c, shl(96, from)) // Store the `from` argument.
+            mstore(0x0c, 0x23b872dd000000000000000000000000) // `transferFrom(address,address,uint256)`.
+            // Perform the transfer, reverting upon failure.
+            if iszero(
+                and(
+                    // The arguments of `and` are evaluated from right to left.
+                    or(eq(mload(0x00), 1), iszero(returndatasize())), // Returned 1 or nothing.
+                    call(gas(), currency, 0, 0x1c, 0x64, 0x00, 0x20)
+                )
+            ) {
+                mstore(0x00, 0x7939f424) // `TransferFromFailed()`.
+                revert(0x1c, 0x04)
+            }
+            mstore(0x60, 0) // Restore the zero slot to zero.
+            mstore(0x40, m) // Restore the free memory pointer.
+        }
     }
 
     /**
@@ -72,7 +134,10 @@ library Currency {
      * @param currency address of token
      * @param account the address to check balance for
      */
-    function balanceOf(address currency, address account) internal view returns (uint256) {
+    function balanceOf(
+        address currency,
+        address account
+    ) internal view returns (uint256) {
         if (currency.isNative()) {
             return account.balance;
         } else {
@@ -89,17 +154,5 @@ library Currency {
      */
     function approve(address currency, address spender, uint256 amount) internal {
         IERC20(currency).approve(spender, amount);
-    }
-
-    function _callOptionalReturnBool(IERC20 token, bytes memory data) private returns (bool) {
-        bool success;
-        uint256 returnSize;
-        uint256 returnValue;
-        assembly ("memory-safe") {
-            success := call(gas(), token, 0, add(data, 0x20), mload(data), 0, 0x20)
-            returnSize := returndatasize()
-            returnValue := mload(0)
-        }
-        return success && (returnSize == 0 ? address(token).code.length > 0 : returnValue == 1);
     }
 }
